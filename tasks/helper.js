07746430fd830ae39b295fa54f45dacc83eb31b8
Mike Dingjan <m.dingjan@lukkien.com>
2013-12-17 10:47:29 +0100

    override findBasePath function because it cannot handle a directory tree with multiple folders with the same name

---
+++ tasks/helper.js
@@ -0,0 +1,36 @@
+module.exports = {
+    
+    init: function( grunt ) {
+        
+        var helper = require( 'grunt-lib-contrib' ).init( grunt ),
+            path = require( 'path' );
+
+        // ==========================================================================================
+        // findBasePath override (original not working when folders in a tree have the same name)
+        // ==========================================================================================
+        helper.findBasePath: function( paths ) {
+
+            if( typeof paths === 'string' && paths.length >= 1 ) {
+                return grunt.util._(path.normalize(paths)).trim(path.sep);
+            }
+
+            paths = grunt.util._.map( basePath, function( file ) {
+                var sections = [];
+                while( (file = path.dirname(file)) !== '.' ) {
+                    sections.push( file );
+                }            
+                return sections;
+            });
+    
+            paths = grunt.util._.intersection.apply( this, paths )
+            return paths[0] || '';
+            
+        };
+        
+
+        // Return the new helper!
+        return helper;
+        
+    };
+    
+};
