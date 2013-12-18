2d1350ce7533ab129070bb12b6ef86991196f189
indieisaconcept <me@indieisaconcept.com>
2013-12-18 23:38:46 +1100

    Tweaked formatting

--- tasks/helper.js
+++ tasks/helper.js
@@ -1,37 +1,36 @@
 module.exports = {
-    
-    init: function( grunt ) {
-        
-        var helper = require( 'grunt-lib-contrib' ).init( grunt ),
-            path = require( 'path' );
 
+    init: function(grunt) {
+
+        var helper = require('grunt-lib-contrib').init(grunt),
+            path = require('path');
 
         // ==========================================================================================
         // findBasePath override (original not working when folders in a tree have the same name)
         // ==========================================================================================
-        helper.findBasePath = function( paths ) {
 
-            if( typeof paths === 'string' && paths.length >= 1 ) {
+        helper.findBasePath = function(/* Array */ paths) {
+
+            if(typeof paths === 'string' && paths.length >= 1) {
                 return grunt.util._(path.normalize(paths)).trim(path.sep);
             }
 
-            paths = grunt.util._.map( paths, function( file ) {
+            paths = grunt.util._.map(paths, function(file) {
                 var sections = [];
-                while( (file = path.dirname(file)) !== '.' ) {
-                    sections.push( file );
-                }            
+                while((file = path.dirname(file)) !== '.') {
+                    sections.push(file);
+                }
                 return sections;
             });
-    
-            paths = grunt.util._.intersection.apply( this, paths )
+
+            paths = grunt.util._.intersection.apply(this, paths);
             return paths[0] || '';
-            
+
         };
-        
 
         // Return the new helper!
         return helper;
-        
+
     }
-    
+
 };
